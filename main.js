3d2fe5789ccf1faf7f857f4dd226b8cce6353bb4
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2025-11-04 12:04:24 +0100

    Initial commit - ajout projet existant

---
+++ main.js
@@ -0,0 +1,8 @@
+const M_BURGER = document.querySelector(".burger")
+const MENU = document.querySelector(".menu")
+
+M_BURGER.addEventListener("click", () => {
+    
+    MENU.classList.toggle('active');
+
+})
